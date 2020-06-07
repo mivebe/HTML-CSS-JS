3faa5c5b507e15ef39ad8581d89aae213bea4c73
Mihail Bezev <45494812+mivebe@users.noreply.github.com>
2020-06-07 08:23:07 +0300

    00l

--- exercises/char counter form.js
+++ exercises/char counter form.js
@@ -1,25 +1,34 @@
+var elTerms = document.getElementById("terms");
 function symbols(e) {
     var text, counter;
     text = document.getElementById("text").value;
-    counter = (200 - text.length);
+    counter = (200 - (text.length + 1));
     if (counter === 0) {
         document.getElementById("text").value = text.slice(0, 199);
     }
     document.getElementById("symbolsLeft").textContent = "Symbols left: " + counter;
-    document.getElementById("lastKey").textContent = "Code of last keypress: " + event.keyCode;
+    document.getElementById("lastKey").textContent = "Last key: " + String.fromCharCode(e.keyCode)
+        + " with code: " + e.keyCode;
 }
 function subscription() {
     var subscription = this.selectedIndex;
     var hui = this.selectedIndex + 1;
     if (subscription == 3) {
+        document.getElementById("subMessage").setAttribute("class", "good")
         document.getElementById("subMessage").innerHTML = "Best choice!";
     } else {
+        document.getElementById("subMessage").setAttribute("class", "warning")
         document.getElementById("subMessage").innerHTML = "Better offer with " +
             this.options[this.selectedIndex + 1].value + " subscription!";
     }
 }
-function checkbox(e) {
-
+function checkbox(event) {
+    console.log("aaaa");
+    if (!elTerms.checked) {
+        console.log("ssss");
+        document.getElementById("termsMessage").innerHTML = "Agree to the terms!";
+        event.preventDefault();
+    }
 }
 document.getElementById("text").addEventListener("keypress", symbols, false);
 document.getElementById("form").addEventListener("submit", checkbox, false);
