55b041e123db7fb0fded3762187f6fa917de4631
Mihail Bezev <45494812+mivebe@users.noreply.github.com>
2020-06-18 03:21:33 +0300

    slider complete

--- exercises/slider.js
+++ exercises/slider.js
@@ -12,7 +12,7 @@ function currentSlide(n) {
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("slides");
-    var dots = document.getElementsByClassName("dot");
+    var dots = document.getElementsByClassName("panel");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
