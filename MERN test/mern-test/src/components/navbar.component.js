5fef807e44af15b392e8cc24500d158c86e88de4
Mihail Bezev <45494812+mivebe@users.noreply.github.com>
2020-06-20 15:34:36 +0300

    mern test complete

--- MERN test/mern-test/src/components/navbar.component.js
+++ MERN test/mern-test/src/components/navbar.component.js
@@ -6,8 +6,8 @@ export default class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-                <Link to="/" className="navbar-brand">ExerTracker</Link>
-                <div className="collapse navbar-collapse">
+                <Link to="/" className="navbar-brand">ExcerTracker</Link>
+                <div className="collpase navbar-collapse">
                     <ul className="navbar-nav mr-auto">
                         <li className="navbar-item">
                             <Link to="/" className="nav-link">Exercises</Link>
