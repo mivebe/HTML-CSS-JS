ca74960913f41e0cc6e81fbd10e1d79bca46a13c
Mihail Bezev <45494812+mivebe@users.noreply.github.com>
2020-10-03 16:54:06 +0300

    scrooge retrieved

--- exercises/Terlik/Scrooge McDuck.js
+++ exercises/Terlik/Scrooge McDuck.js
@@ -1,46 +1,39 @@
 let test = [
-    4, 3,
-    "3 4 4",
-    "2 0 3",
-    "1 1 5",
-    "2 2 5"
+    '4 3',
+    '3 2 4',
+    '2 0 3',
+    '1 1 5',
+    '2 2 5'
 ];
+let print = this.print || console.log;
+let gets = this.gets || ((arr, index) => () => arr[index++])(test, 0);
 
-let print = this.print && console.log;
-let gets = this.gets && ((arr, index) => () => arr[index++])(test, 0);
+const inRange = (value, max) => { return 0 <= value && value < max; }
 
+const input = gets().split(" ");
+const rowsTotal = +input[0];
+const colsTotal = +input[1];
 
-const inRange = (value, max) => {
-    return 0 <= value && value < max;
-}
-const inp = gets().split(" ");
-const rows = +inp[0];
-const cols = +inp[1];
+const matrix = [];
+let row = 0;
+let col = 0;
 
-let startRow = 0;
-let startCol = 0;
-
-const matrix = []
-
-for (let r = 0; r < rows; r++) {
+for (let r = 0; r < rowsTotal; r++) {
     matrix.push([]);
     const next = gets().split(" ");
-    for (let c = 0; c < cols; c++) {
+    for (let c = 0; c < colsTotal; c++) {
         matrix[r][c] = Number(next[c])
         if (matrix[r][c] == 0) {
-            startRow = r;
-            startCol = c;
+            row = r;
+            col = c;
         }
     }
 }
 
 let coins = 0;
-let row = startRow;
-let col = startCol;
 
 const dRows = [0, 0, -1, +1];
 const dCols = [-1, +1, 0, 0];
-
 
 while (true) {
     let tempMax = 0;
@@ -51,7 +44,7 @@ while (true) {
         let nextRow = row + dRows[dir];
         let nextCol = col + dCols[dir];
 
-        if (!inRange(nextRow, rows) || !inRange(nextCol, cols)) {
+        if (!inRange(nextRow, rowsTotal) || !inRange(nextCol, colsTotal)) {
             continue;
         }
 
@@ -72,5 +65,4 @@ while (true) {
     coins++;
 }
 
-print(matrix);
 print(coins);
