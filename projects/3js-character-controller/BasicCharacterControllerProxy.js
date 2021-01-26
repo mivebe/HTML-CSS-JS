a77580b0db793047af5d014c40d39db74c19f356
mivebe <mivebe@abv.bg>
2021-01-27 01:19:10 +0200

    moved stuff to another repo

--- projects/3js-character-controller/BasicCharacterControllerProxy.js
+++
@@ -1,9 +0,0 @@
-export default class BasicCharacterControllerProxy {
-    constructor(animations) {
-        this._animations = animations;
-    }
-
-    get animations() {
-        return this._animations;
-    }
-};
--- projects/gltf attempt/index.js
+++
@@ -1,5 +0,0 @@
-
-let canv = document.createElement("canvas");
-canv.setAttribute("id", "canvasID");
-document.body.appendChild(canv);
-console.log("asd");
