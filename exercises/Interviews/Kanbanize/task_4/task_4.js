b24f19d140ab21203ba30918371dd82db8ce866d
mivebe <mivebe@abv.bg>
2021-09-12 10:57:18 +0300

    tack 4 no library fix

--- exercises/Interviews/Kanbanize/task_4/task_4.js
+++ exercises/Interviews/Kanbanize/task_4/task_4.js
@@ -1,55 +1,15 @@
 
-
 document.getElementById('time-period').addEventListener('change', (event) => {
     event.preventDefault()
+    if (!window.data) { return }
     window.radioValue = event.target.value
     renderChart(getFinalData(window.data))
 })
 
-const renderChart = rowData => {
-    const chart = anychart.bar();
-    chart.data(rowData);
-
-    chart.title("Cards started by date");
-
-    document.getElementById('chart-container').innerHTML = ''
-
-    chart.container("chart-container");
-    chart.draw();
-}
-
-const getFinalData = (arrData) => {
-    const data = arrData.filter(el => el != [] && !el.includes(""))
-    const [, ...rows] = data.filter(el => el[3] != "")
-
-    let dateIdx = 0
-    if (window.radioValue === 'Day' || !window.radioValue) dateIdx = 2
-
-    if (window.radioValue === 'Month') dateIdx = 1
-
-    const dates = rows.map(el => el[3].slice(0, 10).split('-')[dateIdx])
-
-    const datesMap = dates.reduce((acc, curV) => {
-        !acc.has(curV) ? acc.set(curV, 1) : acc.set(curV, (acc.get(curV)) + 1)
-        return acc
-    }, new Map())
-
-    const finalData = []
-    datesMap.forEach((el, key) => {
-        finalData.push([key, el])
-    })
-
-    return finalData
-}
-
-const handleData = arrData => {
-    window.data = arrData
-    const finalData = getFinalData(arrData)
-    renderChart(finalData)
-}
-
 function handleFileChange(event) {
     const file = event.target.files[0]
+
+    document.getElementById("days").checked = true
 
     const reader = new FileReader()
     reader.addEventListener('loadend', ({ target }) => {
@@ -58,7 +18,6 @@ function handleFileChange(event) {
     })
     reader.readAsText(file)
 }
-
 
 function CSVToArray(strData, strDelimiter) {
 
@@ -95,4 +54,70 @@ function CSVToArray(strData, strDelimiter) {
         arrData[arrData.length - 1].push(strMatchedValue);
     }
     return (arrData);
+}
+
+const handleData = arrData => {
+    window.data = arrData
+    const finalData = getFinalData(arrData)
+    renderChart(finalData)
+}
+
+const getFinalData = (arrData) => {
+    const data = arrData.filter(el => el != [] && !el.includes(""))
+    const [, ...rows] = data.filter(el => el[3] != "")
+
+    let dateIdx = 0
+    if (window.radioValue === 'Day' || !window.radioValue) dateIdx = 2
+
+    if (window.radioValue === 'Month') dateIdx = 1
+
+    const dates = rows.map(el => el[3].slice(0, 10).split('-')[dateIdx])
+
+    const datesMap = dates.reduce((acc, curV) => {
+        !acc.has(curV) ? acc.set(curV, 1) : acc.set(curV, (acc.get(curV)) + 1)
+        return acc
+    }, new Map())
+
+    const finalData = []
+    datesMap.forEach((el, key) => {
+        finalData.push([key, el])
+    })
+
+    return finalData
+}
+
+const renderChart = rowData => {
+    const container = document.getElementById("graph_container")
+    container.innerHTML = ''
+
+    const metrics = document.getElementById("metrics")
+    metrics.innerHTML = ''
+
+    const biggest = rowData.reduce((acc, curV) => acc[1] > curV[1] ? acc : curV)
+
+    const sectionWidth = (container.scrollWidth) / (biggest[1] + 1)
+    const sectionHeight = (container.scrollHeight) / (rowData.length + 1)
+
+    rowData.forEach(el => {
+        const width = sectionWidth * el[1]
+
+        const row = document.createElement("div")
+        row.classList.add("row")
+        row.style.width = `${width}px`
+        row.height = `${sectionHeight}px`
+        row.textContent = el[0]
+        container.append(row)
+    })
+
+    for (let i = 0; i < biggest[1] + 1; i++) {
+        const mark = document.createElement("p")
+        mark.textContent = i
+        mark.classList.add("mark")
+        mark.style.width = `${sectionWidth}px`
+        metrics.append(mark)
+    }
+
+    const finalMark = document.createElement("span")
+    finalMark.textContent = biggest[1] + 1
+    metrics.lastChild.appendChild(finalMark)
 }
